16f44a9737ee1355b78867632b25dd491c81e4c9
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2016-03-15 09:36:43 +0200

    init

---
+++ jquery.imghover.js
@@ -0,0 +1,64 @@
+/*!
+ * jQuery Image Hover Effect Plugin 
+ * https://github.com/mertkahyaoglu/jquery-imghover
+ *
+ * Copyright 2014, Mert Kahyaoğlu
+ * version 0.1
+ * MIT license
+ */
+
+(function($, window, undefined) {
+	"use strict";
+
+	$.fn.imghover = function(options) {
+
+		var options = $.extend({}, $.fn.imghover.defaults, options);
+
+		return this.each(function() {
+
+			var $this = $(this);
+			var data = $this.data('imghover').split('|');
+			for (var i = 0; i < data.length; i++) 
+				data[i] = data[i].replace(/\s/g, '');
+
+			$this.css({width:options.width, height: options.height});
+
+			var initial = $('<img/>', {
+				id: 'imghoverInitial',
+			    src: data[0],
+			    style: "position:absolute;height:"+options.height+"px;width:"+options.width+"px;"
+			});
+			var hover = $('<img/>', {
+				id: 'imghoverHover',
+			    src: data[1],
+			    width: options.width,
+			    height: options.height,
+			    style: "display:none;position:absolute;height:"+options.height+"px;width:"+options.width+"px;"
+			});
+
+			$this.append(initial);
+			$this.append(hover);
+
+	        $this.hover(
+	        	function() {
+	        		$this.find('#imghoverHover').stop(true, true).fadeIn(options.delay);
+	        		$this.find('#imghoverInitial').stop(true, true).fadeOut(options.delay);
+	        	},
+	        	function() {
+	        		$this.find('#imghoverHover').stop(true, true).fadeOut(options.delay);
+	        		$this.find('#imghoverInitial').stop(true, true).fadeIn(options.delay);
+	        	}
+	        );
+
+		});
+
+		return $this;
+	};
+
+	$.fn.imghover.defaults = {
+		width: 64,
+		height: 64,
+		delay: 500
+	};
+
+})(jQuery);
